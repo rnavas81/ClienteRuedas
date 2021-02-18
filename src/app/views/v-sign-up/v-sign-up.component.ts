dd314fae03dcffdc7e8de1670e85b5436765a2bd
Under <jorgeolmo.I@gmail.com>
2021-02-18 17:26:50 +0100

    Arreglo vistas

---
+++ src/app/views/v-sign-up/v-sign-up.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-v-sign-up',
+  templateUrl: './v-sign-up.component.html',
+  styleUrls: ['./v-sign-up.component.scss']
+})
+export class VSignUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
