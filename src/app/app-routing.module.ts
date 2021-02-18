b2f12cd7c09b527592e6298db2e2f34d7abd5844
Under <jorgeolmo.I@gmail.com>
2021-02-18 17:27:13 +0100

    Arreglo vistas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { RegistroComponent } from './components/registro/registro.component';
-import { RecuperarPassComponent } from './views/recuperar-pass/recuperar-pass.component';
 import { PerfilUsuarioComponent } from './views/perfil-usuario/perfil-usuario.component';
 import { UnirseRuedaComponent } from './views/unirse-rueda/unirse-rueda.component';
 import { PanelAdministradorComponent } from './views/panel-administrador/panel-administrador.component';
 import { EditarPerfilComponent } from './views/editar-perfil/editar-perfil.component';
+import { VLoginComponent } from './views/v-login/v-login.component';
+import { RecuperarPassComponent } from './views/recuperar-pass/recuperar-pass.component';
+import { VSignUpComponent } from './views/v-sign-up/v-sign-up.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: RegistroComponent },
+  { path: '', component: LoginComponent }, // ESTO DEBERIA SER EL HOME
+  { path: 'login', component: VLoginComponent },
+  { path: 'signup', component: VSignUpComponent },
   { path: 'unirse', component: UnirseRuedaComponent },
   { path:'recuperar', component: RecuperarPassComponent },
   { path:'main', component: PerfilUsuarioComponent },
---
+++ src/app/views/v-login/v-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-v-login',
+  templateUrl: './v-login.component.html',
+  styleUrls: ['./v-login.component.scss']
+})
+export class VLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
